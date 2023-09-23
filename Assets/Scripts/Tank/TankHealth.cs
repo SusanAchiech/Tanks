220d691ae9eda2bb1dc7845333cb1f106b4c4d66
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2023-09-23 19:34:18 +0300

    Enemy tank changes

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
